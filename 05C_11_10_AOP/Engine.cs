5cd4cbe8ba9442d49e152404882c9695f8b9c666
Adriana Gavrilut <gavrilut.adriana@student.uoradea.ro>
2023-11-10 14:54:28 +0200

    curs 05

---
+++ 05C_11_10_AOP/Engine.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05C_11_10_AOP
+{
+    public static class Engine
+    {
+        public static Graphics grp;
+        public static Bitmap bmp;
+        public static PictureBox display;
+        public static Color color = Color.Ivory;
+
+        public static void InitGraph(PictureBox T)
+        {
+            display = T;
+            bmp = new Bitmap(T.Width, T.Height);
+            grp = Graphics.FromImage(bmp);
+            grp.Clear(color);
+        }
+
+        public static void Refresh()
+        {
+            display.Image = bmp;
+        }
+
+        public static int deltaX = 20, deltaY = 20;
+        public static Map demoMap;
+        public static Random rnd = new Random();
+        public static List<Agent> agents = new List<Agent>();
+
+        public static void InitDemo()
+        {
+            demoMap = new Map();
+            demoMap.Init(40, 60);
+
+            for (int i = 0; i < 30; i++)
+            {
+                demoMap.SpawnResources(rnd.Next(40), rnd.Next(60), rnd.Next(5, 100));
+                demoMap.SpawnDeadZone(rnd.Next(40), rnd.Next(60));
+            }
+
+            agents.Add(new Explorer());
+            agents.Add(new Explorer());
+            agents.Add(new Exploiter());
+            agents.Add(new Transporter());
+
+            foreach (Agent agent in agents)
+                SetAgentOnMap(agent, rnd.Next(40), rnd.Next(60));
+        }
+
+        public static void SetAgentOnMap(Agent T, int x, int y)
+        {
+            T.location = new Point(x, y);
+        }
+
+        public static void Draw(Graphics handler)
+        {
+            demoMap.Draw(handler);
+            foreach (Agent agent in agents)
+                agent.Draw(handler);
+        }
+    }
+}
---
+++ 05C_11_10_AOP/Map.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05C_11_10_AOP
+{
+    public class Map
+    {
+        int[,] values;
+        public int n { get { return values.GetLength(0); } }
+        public int m { get { return values.GetLength(1); } }
+
+        public void Init(int n, int m)
+        {
+            values = new int[n, m];
+        }
+
+        public void SpawnResources(int x, int y, int c) // locatie si cantitate
+        {
+            values[x, y] = c;
+        }
+
+        public void SpawnDeadZone(int x, int y)
+        {
+            values[x, y] = -1;
+        }
+
+        public void Draw(Graphics handler)
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    handler.DrawRectangle(Pens.Gray, i * Engine.deltaX, j * Engine.deltaY, Engine.deltaX, Engine.deltaY);
+                    if (values[i, j] == -1)
+                        handler.FillRectangle(new SolidBrush(Color.Black), i * Engine.deltaX, j * Engine.deltaY, Engine.deltaX, Engine.deltaY);
+                    if (values[i, j] > 0)
+                        handler.DrawString(values[i, j].ToString(), new Font("Arial", 10, FontStyle.Regular), new SolidBrush(Color.Red), new PointF(i * Engine.deltaX + 5, j * Engine.deltaY + 5));
+                }
+        }
+
+        public static void LoadFromFile(string fileName)
+        {
+
+        }
+
+        public static void SaveToFile(string fileName)
+        {
+
+        }
+    }
+}
